3abe6a30ce199981f8945523c3e71de053f4591e
Nickolay Polyakov <nikolay.polyakov@ormco.com>
2020-06-03 09:12:11 +0300

    Working STL Viewer

---
+++ src/pages/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+import {TeethCanvas} from "./TeethCanvas";
+import {Navigation} from "./Navigation";
+
+function App() {
+    return (
+        <AppContainer>
+            <AppHeader>
+                <TeethCanvas/>
+                <Navigation/>
+            </AppHeader>
+        </AppContainer>
+    );
+}
+
+
+const AppContainer = styled.div`
+    text-align: center;
+`
+
+const AppHeader = styled.header`
+    background-color: #282c34;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`
+
+
+
+export default App;
---
+++ src/pages/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+import {Button} from "antd-mobile";
+
+const Navigation = () => {
+    return (
+            <AppUi>
+                <AppUiButton>&larr;</AppUiButton>
+                <AppUiButton>&rarr;</AppUiButton>
+            </AppUi>
+    );
+};
+
+const AppUi = styled.div`
+    height: 20vh;
+    width: 100%;
+    background-color: #70e1e1;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    color: aliceblue;
+`
+
+const AppUiButton = styled(Button)`
+    width: 20%;
+    font-size: xx-large;
+`
+
+export {Navigation};
